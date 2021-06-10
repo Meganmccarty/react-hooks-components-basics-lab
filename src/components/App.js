af9d7d7378a0887dc5216b917ecdaa1d96d51e22
Megan McCarty <megan_mccarty@hotmail.com>
2021-06-10 09:00:43 -0400

    commit tree e5cbd599b4c5669c8f5a8519688424aa9eb27e69

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,37 @@
 import React from "react";
 
 function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
+    return (
+        <nav>
+            <a href="#home">I'm a link!</a>
+        </nav>
+    );
 }
 
 function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
+    return (
+        <div id="home">
+            <h1>Home</h1>
+        </div>
+    );
+}
+
+function About() {
+    return (
+        <div id="about">
+            This is just some simple "about me" text!
+        </div>
+    )
 }
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* write an <About> component here */}
-    </div>
-  );
+    return (
+        <div>
+            <NavBar />
+            <Home />
+            <About />
+        </div>
+    );
 }
 
 export default App;
